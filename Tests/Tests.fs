b31076718c36d9c4abef591ccfc7ab123e4b656d
Sang Kil Cha <sangkilc@kaist.ac.kr>
2025-03-03 14:28:17 +0900

    Use .NET 9

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -9,24 +9,24 @@ type TestClass () =
   [<TestMethod; TestCategory "1">]
   member _.``Test1`` () =
     let result = Math.collatz 9
-    Assert.AreEqual (19, result)
+    Assert.AreEqual<int> (19, result)
 
   [<TestMethod; TestCategory "2">]
   member _.``Test2`` () =
     let result = Math.collatz 27
-    Assert.AreEqual (111, result)
+    Assert.AreEqual<int> (111, result)
 
   [<TestMethod; TestCategory "3">]
   member _.``Test3`` () =
     let result = Math.collatz 871
-    Assert.AreEqual (178, result)
+    Assert.AreEqual<int> (178, result)
 
   [<TestMethod; TestCategory "4">]
   member _.``Test4`` () =
     let result = Math.collatz 6171
-    Assert.AreEqual (261, result)
+    Assert.AreEqual<int> (261, result)
 
   [<TestMethod; TestCategory "5">]
   member _.``Test5`` () =
     let result = Math.collatz 670617279
-    Assert.AreEqual (986, result)
+    Assert.AreEqual<int> (986, result)
